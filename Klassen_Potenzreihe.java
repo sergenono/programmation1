fe67d696afa580e842afd44c12a5f349c3f82294
sergenono <82665960+sergenono@users.noreply.github.com>
2023-08-23 21:23:23 +0200

    Add files via upload

--- Klassen_Potenzreihe.java
+++ Klassen_Potenzreihe.java
@@ -4,7 +4,7 @@ public class Klassen_Potenzreihe {
 	
 	// ln(1 + x) =  x - (x^2 / 2) +  (x^3 /3 ) - (x^4 / 4 ) + (x^5 /5 )......
 	
-	 static double exponent  = 6;
+	 static double exponent  = 5;
 	 
 	
 			
@@ -16,6 +16,8 @@ public class Klassen_Potenzreihe {
 	{
 		  double result = 0 ;
 		  
+		  double  anfangsresult ;
+		  
 		  double num  = 1 ;
 			 
 			 double denom = 1 ;
@@ -23,13 +25,13 @@ public class Klassen_Potenzreihe {
 			 int sign = -1 ;
 			double y ;
 		   
-		   
+		   double recup = 0 ;
 	      if(x  > - 1 &&  x <= 1 )
 	      {
 	    	  for(int i = 1  ; i <= exponent ; i++)
 	    	  {
 	    		 
-	    		 
+	    		 anfangsresult  = result ;
 	    		  
 	    		  if(i % 2 !=0 )
 	    		  {
@@ -45,19 +47,37 @@ public class Klassen_Potenzreihe {
 	    				   denom  =  denom + 1;
 	    				  result =  result + (-sign) *(num/denom)* x;
 	    			  }
+	    		System.out.println(result);
 	    		  }
 	    		  else 
 	    		  {
                          denom =  denom +1 ;
 	    			  
 	    			  result =  result + sign *(num/denom)* x ;
+	    		System.out.println(result);
 	    		  }
+	    		  
+	    		  
+    		  
 	    		  x *= x ;
+	    		  
+	    		  
+	    		  if((result- anfangsresult)<=eps)
+	 	    		 
+{
+	recup =  result ;
+}
+	
+	    	/*else 
+	    		  {
+	    			  System.out.println("result - anfangsresult > eps" ) ;
+	    		  }  */
+
 	    	  }
 	    	  
-	    	 //esult = result;
+	    	 
 	    	  
-	    	  System.out.println(result);
+	    	System.out.println(recup);
 	    
 	    	 
 	      }
@@ -73,7 +93,7 @@ public class Klassen_Potenzreihe {
 	
 public static void main(String[] args) {
 		
-	//lassen_Potenzreihe p =  new Klassen_Potenzreihe();
+	
 		 double end = ln1plusX(1, 0.00000001);
 	}
 
