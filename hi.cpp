447fbe270057204b8773582f46583cee8c722524
sergenono <82665960+sergenono@users.noreply.github.com>
2023-10-25 18:49:52 +0200

    Add files via upload

--- hi.cpp
+++ hi.cpp
@@ -8,47 +8,159 @@
 
 #include <iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 constexpr unsigned int N{2};
+int n = N  ;
 
-int rows = N;
-	int cols = N ;
+int a  ;
+int b ;
 
-	 int n ;
-void eingabe( std::vector<std::vector<double>>& matrx1);
+void eingabe( std::vector<std::vector<double>>   &matrx1  , std::vector<std::vector<double>>  &matrx2);
+void mult( std::vector<std::vector<double>>   &matrx1  , std::vector<std::vector<double>>  &matrx2 , std::vector<std::vector<double>> &result);
 
 int main() {
-	std::vector<std::vector<int>> myMatrx1 ;
-	std::vector<std::vector<int>>myMatrx2;
+	std::vector<std::vector<double>>  myMatrx1 ;
+	std::vector<std::vector<double>> myMatrx2;
+	std::vector<std::vector<double>> res ;
+
+	eingabe(myMatrx1 , myMatrx2);
+	mult(myMatrx1 , myMatrx2 , res) ;
 
 
-
-	std::cout<<"enter items :";
-	for(int i = 0 ; i < rows ; i++)
-	{
-		for(int j = 0 ; j < cols ; j++)
-		{
-			std::cin>> n ;
-
-			std::cout<<myMatrx1[i][j];
-		}
-	}
-
-
-	eingabe( myMatrx1);
 	return 0;
 }
-void eingabe(std::vector<std::vector<double>>& matrx1 )
+void eingabe(std::vector<std::vector<double>> &matrx1  , std::vector<std::vector<double>>  &matrx2)
 {
-	std::cout<<"the elements in vectors are";
 
-	 for(int i = 0 ; i < rows ; i++)
+	 for(int i = 0 ; i < n ; i++)
 	 {
 		 std::vector<double> vec ;
-		   for(int j = 0 ; j < cols  ; j++)
+		   for(int j = 0 ; j < n  ; j++)
 		   {
-			   vec.push_back(n);
+			   std::cout<<"enter a number" ;
+					   std::cin>> a ;
+					   vec.push_back(a);
 		   }
+
 		   matrx1.push_back(vec);
 	 }
+	 for(int i = 0 ; i < n ; i++)
+	 {
+		 std::vector<double> vec ;
+		   for(int j = 0 ; j < n  ; j++)
+		   {
+			   std::cout<<"enter  number" ;
+					   std::cin>> b ;
+			   vec.push_back(b);
+		   }
+		   matrx2.push_back(vec);
+	 }
+	 for(int i = 0 ; i < n ; i++)
+	 {
+		 for(int j = 0 ; j < n ; j++)
+		 {
+			 std::cout<< matrx1[i][j];
+		 }
+		 std::cout<<"\n" ;
+	 }
+
+	 std::cout<<"\n" ;
+
+	 for(int i = 0 ; i < n ; i++)
+		 {
+			 for(int j = 0 ; j < n ; j++)
+			 {
+				 std::cout<< matrx2[i][j];
+			 }
+			 std::cout<<"\n" ;
+		 }
 }
+	 void mult( std::vector<std::vector<double>>   &matrx1  , std::vector<std::vector<double>>  &matrx2 , std::vector<std::vector<double>> &result)
+	 {
+		 for(int i = 0 ; i < n  ; i++)
+		 	{
+		 		   std::vector<double> vec ;
+
+		 		for(int j = 0 ; j < n ; j++)
+		 		{
+		 			std::cout<<"enter a number";
+		 			std::cin>> a ;
+		 		      vec.push_back(a);
+
+		 		}
+		 		matrx1.push_back(vec);
+		 	}
+		 	for(int i = 0 ; i < n  ; i++)
+		 	{
+		 		   std::vector<double> vec ;
+
+		 		for(int j = 0 ; j < n ; j++)
+		 		{
+		 			std::cout<<"enter number";
+		 			std::cin>> b ;
+		 		      vec.push_back(b);
+
+		 		}
+		 		matrx2.push_back(vec);
+		 	}
+
+		 	for(int i = 0 ; i < n ; i++)
+		 	{
+		 		for(int j = 0 ; j< n ; j++)
+		 		{
+		 			std::cout<<matrx1[i][j] ;
+		 		}
+
+		 		std::cout<<"\n";
+		 	}
+		 	for(int i = 0 ; i < n ; i++)
+		 	{
+		 		for(int j = 0 ; j< n ; j++)
+		 		{
+		 			std::cout<<matrx2[i][j] ;
+		 		}
+
+		 		std::cout<<"\n";
+		 	}
+
+		     int laengeRows = matrx1[0].size();
+		      int laengeCols = matrx2[0].size();
+		      if(laengeRows != laengeCols)
+		      {
+		     	 std::cout<<"beide matrix koennen nicht multipliziert werden " ;
+		      }
+		      else
+		      {
+		     	 for(int i = 0 ; i < n*n ; i++)
+		     	 {
+
+		     		 std::vector<double>rows ;
+		     		 for(int j = 0 ; j < n ; j++)
+		     		 {
+		     		 int tmp  = 0 ;
+		                     for(int k = 0 ; k< n ; k++)
+		                     {
+		                     	 tmp  =  tmp + (matrx1[i][k]*matrx2[k][j]) ;
+
+		                     }
+		                	 rows.push_back(tmp);
+
+		     		 }
+
+		     	     result.push_back(rows);
+		     	 }
+		      }
+		      for(int i = 0 ; i < n ; i++)
+		      {
+		     	 for(int j = 0 ; j < n ; j++ )
+		     	 {
+		     		 std::cout << result[i][j]  << " ";
+		     	 }
+		     	 std::cout<<"\n";
+		      }
+		      //std::cout<< laengeRows ;
+
+	 }
+
+
