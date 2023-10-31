01b9f66975d80d6e7740f773accdda570e26ccef
sergenono <82665960+sergenono@users.noreply.github.com>
2023-10-31 03:48:59 +0100

    Add files via upload

--- Auf2blatt2.java
+++ Auf2blatt2.java
@@ -7,15 +7,15 @@ public class Auf2blatt2 {
 		
 		
 		double a = (16.0/9.0)*(16.0/9.0) ;
-		double b = 3.0 * (1.0 /8.0);       // funktioniert nicht 
+		double b = 3.0 * (1.0 /8.0);       //  
 		double d = Math.sqrt(10) ;
 		double c = 3.0 *(1.0/7.0);
 		
 		System.out.printf("%.15g%n", Math.PI);     // funktioniert 
-		System.out.printf("%.15g%n"  , a );
-		System.out.printf("%.15g%n", b);
-		System.out.printf("%.15g%n", c);
-		System.out.printf("%.15g%n", d);
+		System.out.printf("%.15g%n"  , a );         // funktioniert nicht 
+		System.out.printf("%.15g%n", b);              // funktioniert nicht 
+		System.out.printf("%.15g%n", c);             // funktioniert nicht 
+		System.out.printf("%.15g%n", d);             // funktioniert 
 
 	}
 
