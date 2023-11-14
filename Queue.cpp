fc1620af4b642b64b0e2e8ee2d93ddae00a65eb6
sergenono <82665960+sergenono@users.noreply.github.com>
2023-11-14 06:42:01 +0100

    Add files via upload

---
+++ Queue.cpp
@@ -0,0 +1,80 @@
+/*
+ * Queue.cpp
+ *
+ *  Created on: 8 nov. 2023
+ *      Author: kiano
+ */
+
+#include "Queue.h"
+#include<memory>
+#include<algorithm>
+using namespace std ;
+Queue::Queue() {
+
+	 pDequeue = NULL ;
+	 pEnqueue = NULL ;
+	 size = 0 ;
+
+
+
+}
+
+/*Queue::~Queue() {   default weil unique_ptr
+
+}*/
+Queue::Queue(QueueItem other)
+{
+	pDequeue = std::make_unique<QueueItem>(other) ;
+	pEnqueue = new QueueItem[other] ;
+	size  = 0;
+}
+Queue::QueueItem::QueueItem()
+{
+	value = 0 ;
+	pYounger = NULL ;
+}
+Queue::QueueItem::QueueItem(int val )
+{
+	value = val ;
+	pYounger = std::make_unique<int>(value) ;
+}
+void Queue::enqueue(QueueItem &other)
+{
+     int *front ;
+     int *rear ;
+     if(size == 0)
+     {
+        // front  =std:: move(pEnqueue) ;
+          pEnqueue = pDequeue.get();
+     }
+     else
+     {
+    	 pDequeue =   pDequeue.get() ;
+     }
+}
+int dequeue()
+{
+
+}
+
+/*Queue::Queue(const Queue &other) {
+	// TODO Auto-generated constructor stub       default weil unique_ptr
+
+}*/
+
+/*Queue::Queue(Queue &&other) {
+	// TODO Auto-generated constructor stub   default weil unique_ptr
+
+}*/
+
+Queue& Queue::operator=(const Queue &other) {
+	// TODO Auto-generated method stub
+	return *this ;
+
+}
+
+/*Queue& Queue::operator=(Queue &&other) {
+	// TODO Auto-generated method stub               default  weil unique pointer
+
+}*/
+
