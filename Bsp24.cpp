8b4db0adc5558f09c3f02434784cb3e4fa8d805a
sergenono <82665960+sergenono@users.noreply.github.com>
2023-11-17 06:24:31 +0100

    Add files via upload

---
+++ Bsp24.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : Bsp24.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include "Komplex.h"
+#include<iostream>
+#include<algorithm>
+using namespace std ;
+#include <iostream>
+using namespace std;
+
+int main() {
+	Komplex  k1{1.0 ,3.0} ;
+	Komplex k2{3.0 , 1.0} ;
+
+	cout << k1 << endl; // prints !!!Hello World!!!
+	return 0;
+}
