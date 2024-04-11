b59a697d9f8a2269714acd2d3fa626f0645468c5
sergenono <82665960+sergenono@users.noreply.github.com>
2024-04-11 06:41:45 +0200

    Add files via upload

---
+++ NegativeNumberException.java
@@ -0,0 +1,18 @@
+package sur;
+
+public class NegativeNumberException extends Exception{
+	NegativeNumberException()
+	{
+		
+	}
+	 NegativeNumberException(String message)
+	 {
+		 super(message);
+	 }
+	 
+	
+	 }
+
+
+
+
